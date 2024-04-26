71fec1bc0e4483021429692ae4d0dab9067c38e1
Ogbo0810 <59059600+Ogbo0810@users.noreply.github.com>
2024-04-26 09:54:03 -0400

    Update timing_constraint_plan1_baseline.tcl

--- Synth/timing_constraint_plan1_baseline.tcl
+++ Synth/timing_constraint_plan1_baseline.tcl
@@ -1,4 +1,19 @@
-# Define clocks with their frequencies for Clock Plan #1 for Base Logic Design
+//-----------------------------------------------------------------------------------
+// # Author: Alberta Ogbo Dadeboe (albertaod@vt.edu)
+// # Group5 Project: Advance VLSI Design, ECE5545 Spring2024 
+//-----------------------------------------------------------------------------------
+// # distributed under the mit license
+// # https://opensource.org/licenses/mit-license.php
+//-----------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------
+// # Defining timing constraints for Clock Plan #1 for basic logic design : 
+// # Clock Frequencies in MHz (Global/M/S 0 - M/S 1 - M/S#2 - M/S 3): 200-100-50-25
+// # Setup time for input ports - 30ps
+// # Hold time for output ports - 10ps
+//-----------------------------------------------------------------------------------
+
+
 create_clock -name master_clk_0 -period 5 [get_ports {mst0_aclk}]  ;# 200 MHz
 create_clock -name master_clk_1 -period 10 [get_ports {mst1_aclk}] ;# 100 MHz
 create_clock -name master_clk_2 -period 20 [get_ports {mst2_aclk}] ;# 50 MHz 
